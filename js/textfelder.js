322af6a706c54bbed553c0514c3be503534eebd8
Patric Auer <patric.auer@googlemail.com>
2014-05-11 20:24:57 +0200

    change: detail.html, detail.css, textfelder.js
    Absätze innerhalb der Dateien entfernt

--- js/textfelder.js
+++ js/textfelder.js
@@ -1,10 +1,8 @@
-function test(){
+function test() {
     alert('Hallo Welt');
-
 }
 
-    function wechsle(a,b)
-    {
-        top['Index'].document.location.href = a;
-        top['Detail'].document.location.href = b;
-    }
+function wechsle(a, b) {
+    top['Index'].document.location.href = a;
+    top['Detail'].document.location.href = b;
+}
